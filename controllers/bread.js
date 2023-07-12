0d0160be88d8a76464a37780cbd5b6776372039a
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-07-12 19:54:44 -0400

    Add functionality

--- controllers/bread.js
+++ controllers/bread.js
@@ -35,6 +35,30 @@ router.post('/', (req, res) => {
     res.status(303).redirect('/breads')
 })
 
+// GET edit page
+router.get('/:index/edit', (req, res) => {
+    const { index } = req.params
+    res.render('edit', {
+        bread: Bread[index],
+        index
+    })
+})
+
+// PUT update to a bread by index
+router.put('/:index', (req, res) => {
+    const { index } = req.params
+    if(req.body.hasGlutten === 'on') {
+        req.body.hasGluten = true
+    }   else {
+        req.body.hasGluten = false
+    }
+
+    if(!req.body.img) req.body.image = 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'                      
+
+    Bread[index] = req.body
+    res.status(303).redirect(`/breads/${index}`)
+})
+
 router.delete('/:index', (req, res) => {
     const { index } = req.params
     Bread.splice(index, 1)
--- views/show.jsx
+++ views/show.jsx
@@ -16,6 +16,9 @@ function Show ({bread, index}) {
             have glutten
         </p>
         <img src={bread.image} alt={bread.name} />
+        <a href={`/breads/${index}/edit`} >
+            <button>Edit</button>
+          </a>
       <form action={`/breads/${index}?_method=DELETE`} method='POST' >
         <input type='submit' value='DELETE' />
       </form>
